bab889bdaa3618a453abcd05b7ba512b58ff6fd4
Caio Croccia <caiocroccia22@gmail.com>
2026-05-10 19:17:33 -0300

    feat: implement Product class

---
+++ ProductService/src/main/java/br/com/ccroccia/vendas/online/ProductService/domain/Products.java
@@ -0,0 +1,53 @@
+package br.com.ccroccia.vendas.online.ProductService.domain;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder; //implement build desing pattern
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Builder
+@NoArgsConstructor(force = true)
+@Getter
+@Setter
+@Schema(name = "Products", description = "Products")
+public class Products {
+
+    @Id
+    @NotNull
+    @NotEmpty
+    @Schema(name = "Unique register")
+    private final int id;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 60)
+    @Schema(description = "Name")
+    private final String name;
+
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 60)
+    @Schema(description = "Type")
+    private final String type;
+
+    @NotNull
+    @Schema(description = "Price")
+    private final float price;
+
+
+    public Products(int id, String name, String type, float price){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+}
